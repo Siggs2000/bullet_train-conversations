dc539810b83efcf047e4e0227facff06741fe142
Seth Siegler <usaseth@gmail.com>
2023-12-10 16:24:26 -0800

    very temporary removal of account_load_and_authorize_resource

--- app/controllers/account/conversations_controller.rb
+++ app/controllers/account/conversations_controller.rb
@@ -1,10 +1,7 @@
-require "bullet_train/super_load_and_authorize_resource/version"
-require "bullet_train/super_load_and_authorize_resource/engine"
-
 class Account::ConversationsController < Account::ApplicationController
   include Conversations::BaseController
 
-  account_load_and_authorize_resource :conversation, through: BulletTrain::Conversations.parent_association, through_association: :conversations, except: [:create]
+  # account_load_and_authorize_resource :conversation, through: BulletTrain::Conversations.parent_association, through_association: :conversations, except: [:create]
 
   def author
     :membership
--- app/controllers/participants/conversations_controller.rb
+++ app/controllers/participants/conversations_controller.rb
@@ -1,11 +1,7 @@
-
-require "bullet_train/super_load_and_authorize_resource/version"
-require "bullet_train/super_load_and_authorize_resource/engine"
-
 class Participants::ConversationsController <  BulletTrain::Conversations.participant_parent_controller.constantize
   include ::Conversations::BaseController
 
-  account_load_and_authorize_resource :conversation, through: BulletTrain::Conversations.participant_parent_association, through_association: :conversations, except: [:create]
+  # account_load_and_authorize_resource :conversation, through: BulletTrain::Conversations.participant_parent_association, through_association: :conversations, except: [:create]
 
   def author
     :participant
